b516aa0ad266d4fa010f31bb395a50182263a7ab
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-03-30 14:45:00 -0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -33,3 +33,27 @@ bb = df
 bb['bb_h'] = indicator_bb.bollinger_hband()
 bb['bb_l'] = indicator_bb.bollinger_lband()
 bb = bb[['Close','bb_h','bb_l']]
+
+# Resistence Strength Indicator
+rsi = RSIIndicator(df['Low']).rsi()
+
+###################
+# Set up main app #
+###################
+
+# Plot the prices and the bolinger bands
+st.write('Stock Bollinger Bands')
+st.line_chart(bb)
+
+progress_bar = st.progress(0)
+
+# Plot RSI
+st.write('Stock RSI ')
+st.line_chart(rsi)
+
+# Data of recent days
+st.write('Recent data ')
+st.dataframe(df.tail(30))
+
+
+
