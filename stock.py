c532f42e586ef941255a704d25d0daa87a786ca4
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-03-30 14:32:54 -0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import datetime
+import yfinance as yf
 
 st.write("""
 ## Closing Price
@@ -11,10 +12,16 @@ st.write("""
 option = st.sidebar.selectbox('Select one symbol', ( 'AAPL', 'MSFT',"SPY",'WMT'))
 import datetime
 today = datetime.date.today()
-before = today - datetime.timedelta(days=700)
+before = today - datetime.timedelta(days=1825)
 start_date = st.sidebar.date_input('Start date', before)
 end_date = st.sidebar.date_input('End date', today)
 if start_date < end_date:
     st.sidebar.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
 else:
     st.sidebar.error('Error: End date must fall after start date.')
+###############
+#Download Data#
+###############
+df = yf.download(option,start= start_date,end= end_date, progress=False)
+
+    
