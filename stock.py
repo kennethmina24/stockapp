ce6a3bec5b5746b6addb340a174ca249327b127d
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-03-30 14:27:31 -0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import datetime
 
 st.write("""
 ## Closing Price
 """)
 
-option = st.sidebar.selectbox('Select one symbol', input())
+###########
+# sidebar #
+###########
+option = st.sidebar.selectbox('Select one symbol', ( 'AAPL', 'MSFT',"SPY",'WMT'))
+import datetime
+today = datetime.date.today()
+before = today - datetime.timedelta(days=700)
+start_date = st.sidebar.date_input('Start date', before)
+end_date = st.sidebar.date_input('End date', today)
+if start_date < end_date:
+    st.sidebar.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+else:
+    st.sidebar.error('Error: End date must fall after start date.')
