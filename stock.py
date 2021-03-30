12bd60c663d683821e3ed47264edb0f345a07090
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-03-30 13:21:44 -0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -10,8 +10,6 @@ st.write("""
 Shown are the stock **closing price** and ***volume*** of Google!
 """)
 
-option = st.sidebar.selectbox(input(), ( 'MVIS', 'APHA',"XPEV","PLTR"))
-
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 tickerSymbol = 'GOOGL'
