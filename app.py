be5f68dc37083751f981812902f7ac294bd64e9b
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-04 16:20:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import stock,NBA
 
 app = MultiApp()
 
-#st.markdown("""
-# Multi-Page App
-#This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
-#""")
+st.markdown("""
+Multi-Page App
+This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+""")
 
 # Add all your application here
-#app.add_app("Stock", stock.app)
+app.add_app("Stock", stock.app)
 #app.add_app("NBA", NBA.app)
 # The main a
