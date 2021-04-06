3730aa73bfd54fbfc91d1573adcac6bd4aa5a5b3
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-06 19:13:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from multiapp import MultiApp
 app = MultiApp()
 
 st.markdown("""
-Multi-Page App
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+Computer Science-Automate My Life
 """)
 
 # Add all your application here
