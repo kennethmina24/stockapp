8797e51317894ad44e8dcf852e15ab9b3d0df864
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-06 18:46:43 -0400

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -1,18 +1,21 @@
 import streamlit as st
 
 class MultiApp:
+
     def __init__(self):
         self.apps = []
+
     def add_app(self, title, func):
+    
         self.apps.append({
             "title": title,
             "function": func
         })
-     def run(self):
+
+    def run(self):
         app = st.sidebar.radio(
             'Go To',
             self.apps,
             format_func=lambda app: app['title'])
-        app['function']()
 
-   
+        app['function']()
