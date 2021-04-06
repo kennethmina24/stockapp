7c8dcfedd285ce079d41462a369075ff2e28a83b
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-06 18:52:38 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -2,12 +2,13 @@ import yfinance as yf
 import streamlit as st
 
 def app():
-  st.write("""# Simple Stock Price App Shown are the stock closing price and volume of Google!""")
-  tickerSymbol = 'GOOGL'
-  tickerData = yf.Ticker(tickerSymbol)
-  tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-  st.line_chart(tickerDf.Close)
-  st.line_chart(tickerDf.Volume)
+   st.title('Data Stats')
+  #st.write("""# Simple Stock Price App Shown are the stock closing price and volume of Google!""")
+  #tickerSymbol = 'GOOGL'
+  #tickerData = yf.Ticker(tickerSymbol)
+  #tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+  #st.line_chart(tickerDf.Close)
+  #st.line_chart(tickerDf.Volume)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
