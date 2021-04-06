9056163451df606119348a57f5e856d28a487f6a
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-06 19:10:06 -0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -14,60 +14,59 @@ def app():
     tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list) # Select ticker symbol
     tickerData = yf.Ticker(tickerSymbol) # Get ticker data
 
+    #Ticker information
+    string_logo = '<img src=%s>' % tickerData.info['logo_url']
+    st.markdown(string_logo, unsafe_allow_html=True)
 
-# Ticker information
-#string_logo = '<img src=%s>' % tickerData.info['logo_url']
-#st.markdown(string_logo, unsafe_allow_html=True)
+    string_name = tickerData.info['longName']
+    st.header('**%s**' % string_name)
 
-#string_name = tickerData.info['longName']
-#st.header('**%s**' % string_name)
+    string_summary = tickerData.info['longBusinessSummary']
+    st.info(string_summary)
 
-#string_summary = tickerData.info['longBusinessSummary']
-#st.info(string_summary)
+    ###########
+    # sidebar #
+    ###########
+    import datetime
+    today = datetime.date.today()
+    before = today - datetime.timedelta(days=1825)
+    start_date = st.sidebar.date_input('Start date', before)
+    end_date = st.sidebar.date_input('End date', today)
+    if start_date < end_date:
+        st.sidebar.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+    else:
+        st.sidebar.error('Error: End date must fall after start date.')
+    ###############
+    #Download Data#
+    ###############
+    df = yf.download(tickerSymbol, start= start_date,end= end_date, progress=False)
+    #Indicators#
+    # Bollinger Bands#
+    indicator_bb = BollingerBands(df['Low'])
+    bb = df
+    bb['bb_h'] = indicator_bb.bollinger_hband()
+    bb['bb_l'] = indicator_bb.bollinger_lband()
+    bb = bb[['Low','bb_h','bb_l']]
 
-###########
-# sidebar #
-###########
-#import datetime
-#today = datetime.date.today()
-#before = today - datetime.timedelta(days=1825)
-#start_date = st.sidebar.date_input('Start date', before)
-#end_date = st.sidebar.date_input('End date', today)
-#if start_date < end_date:
-#    st.sidebar.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
-#else:
-#    st.sidebar.error('Error: End date must fall after start date.')
-###############
-#Download Data#
-###############
-#df = yf.download(tickerSymbol, start= start_date,end= end_date, progress=False)
-#Indicators#
-# Bollinger Bands#
-#indicator_bb = BollingerBands(df['Low'])
-#bb = df
-#bb['bb_h'] = indicator_bb.bollinger_hband()
-#bb['bb_l'] = indicator_bb.bollinger_lband()
-#bb = bb[['Low','bb_h','bb_l']]
+    # Resistence Strength Indicator
+    rsi = RSIIndicator(df['Low']).rsi()
 
-# Resistence Strength Indicator
-#rsi = RSIIndicator(df['Low']).rsi()
+    ###################
+    # Set up main app #
+    ###################
 
-###################
-# Set up main app #
-###################
+    # Data of recent days
+    st.write('Recent data ')
+    st.dataframe(df.tail(15))
 
-# Data of recent days
-#st.write('Recent data ')
-#st.dataframe(df.tail(15))
+    # Plot the prices and the bolinger bands
+    st.write('Stock Bollinger Bands')
+    st.line_chart(bb)
+    progress_bar = st.progress(0)
 
-# Plot the prices and the bolinger bands
-#st.write('Stock Bollinger Bands')
-#st.line_chart(bb)
-#progress_bar = st.progress(0)
-
-# Plot RSI
-#st.write('Stock RSI ')
-#st.line_chart(rsi)
-#st.dataframe(rsi.tail(7))
+    # Plot RSI
+    st.write('Stock RSI ')
+    st.line_chart(rsi)
+    st.dataframe(rsi.tail(7))
 
 
