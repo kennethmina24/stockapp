6bf458162d240efb2d72821cdcb04fd4c9515155
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-09 17:26:16 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -5,12 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def app():
-   st.set_page_config(layout="wide")
-   sidebar = st.sidebar
-   leftpanel, rightpanel = st.beta_columns([7, 3])
-
-   Players = pd.DataFrame(config.Players, columns=['Name', 'Url'])
-   Player_list = Players.Name.tolist()
+   leftpanel.title("Player Comparison")
 
    
 
