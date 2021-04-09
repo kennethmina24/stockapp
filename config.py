b240e3873879a9433b50632ae86dafe70bc50acd
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-09 17:04:19 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,48 @@
+URL = "https://www.basketball-reference.com/players/"
+
+Players = {"Name": ["Michael Jordan", "Lebron James", "Kobe Bryant", "Larry Bird", "Magic Johnson",
+                    "Hakeem Olajuwon", "Kareem Abdul-Jabbar", "Shaquille O'Neal", "Wilt Chamberlain", "Bill Russell"],
+
+           "Url": ["/j/jordami01.html",
+                   "j/jamesle01.html",
+                   "b/bryanko01.html",
+                   "b/birdla01.html",
+                   "j/johnsma02.html",
+
+                   "o/olajuha01.html",
+                   "a/abdulka01.html",
+                   "o/onealsh01.html",
+                   "c/chambwi01.html",
+                   "r/russebi01.html"]
+           }
+
+Glossary = {"Name": ["FG%", "3P%", "FT%", "PTS", "STL", "FT", "G", "GS", "MP", "FG", 
+                     "FGA", "3P", "3PA", "2P", "2PA", "2P%", "eFG%", "FTA", "ORB", "DRB", 
+                     "TRB", "AST", "BLK", "TOV", "PF"],
+
+            "Meaning": ["Field Goal Percentage",
+                        "3-Point Field Goal Percentage",
+                        "Free Throw Percentage",
+                        "Points Per Game",
+                        "Steals Per Game",
+                        "Free Throws Per Game",
+                        "Games",
+                        "Games Started",
+                        "Minutes Played Per Game",
+                        "Field Goals Per Game",
+                        "Field Goal Attempts Per Game",                         
+                        "3-Point Field Goals Per Game",
+                        "3-Point Field Goal Attempts Per Game",                        
+                        "2-Point Field Goals Per Game",
+                        "2-Point Field Goal Attempts Per Game",
+                        "2-Point Field Goal Percentage",
+                        "Effective Field Goal Percentage",
+                        "Free Throw Attempts Per Game",                        
+                        "Offensive Rebounds Per Game",
+                        "Defensive Rebounds Per Game",
+                        "Total Rebounds Per Game",
+                        "Assists Per Game",
+                        "Blocks Per Game",
+                        "Turnovers Per Game",
+                        "Personal Fouls Per Game",                        
+                         ]}
