c5bf47387a8fd9039f39c9a4b9c24843ebea4f35
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-11 13:17:00 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -6,11 +6,12 @@ import seaborn as sns
 
 def app():
       # Sidebar - Position selection
+      st.title('NBA Player Explorer')
       unique_pos= ['C','PF','SF','PG','SG']
       selected_pos= st.sidebar.multiselect('Position', unique_pos, unique_pos)
       #File Upload
-      st.subheader("Dataset")
-      st.file_uploader("Upload CSV",type=['csv'])
+      st.subheader("DataFrame")
+      st.file_uploader("Upload File",type=['csv','txt','docx','pdf'])
 
 
 
