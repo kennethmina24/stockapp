553edd7e78a2b7b3dfe45b2e3488a26f60a71a5c
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-11 15:41:12 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -9,11 +9,13 @@ def app():
       unique_pos= ['C','PF','SF','PG','SG']
       selected_pos= st.sidebar.multiselect('Position', unique_pos, unique_pos)
       #File Upload
-      def main():
-            st.subheader("DataFrame")
-            data_file=st.file_uploader("Upload File",type=['csv','txt','docx','pdf'])
-            df = pd.read_csv(data_file)
-            st.dataframe(df)
+      st.subheader("DataFrame")
+      data_file=st.file_uploader("Upload File",type=['csv','txt','docx','pdf'])
+      df = pd.read_csv(data_file)
+      file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+	#Show Data
+      st.write(file_details)
+      st.dataframe(df)
       
       
 
