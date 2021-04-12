f2117ed73a3e360371e3aef06a51e633f4c1cbb4
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-12 14:03:25 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -27,19 +27,19 @@ def app():
       playerstats = load_data(selected_year)
       
       data_file=st.file_uploader("Upload File",type=['csv','txt','docx','pdf'])
-      if data_file is None:
-            st.write("Enter File, the data set is empty")
-      if data_file is not None:
-            df = pd.read_csv(data_file)
-            file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
-                     #Show Data
-            st.write(file_details)
+      #if data_file is None:
+      #      st.write("Enter File, the data set is empty")
+      #if data_file is not None:
+      #      df = pd.read_csv(data_file)
+      #      file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+                   #Show Data
+      #    st.write(file_details)
             #st.dataframe(df)
                      # Filtering data
-            unique_pos= ['C','PF','SF','PG','SG']
-            selected_pos= st.sidebar.multiselect('Position', unique_pos, unique_pos)       
-            df_selected_team = df[(df.Position.isin(selected_pos))]
-            st.dataframe(df_selected_team)
+       #     unique_pos= ['C','PF','SF','PG','SG']
+       #     selected_pos= st.sidebar.multiselect('Position', unique_pos, unique_pos)       
+       #     df_selected_team = df[(df.Position.isin(selected_pos))]
+       #    st.dataframe(df_selected_team)
       
       
       
