846d0cebcfe1f7ce44370786cbd1e06bc3268fcf
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-12 12:58:18 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
 def app():
       # Sidebar - Position selection
