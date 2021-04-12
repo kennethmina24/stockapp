3d936a618e6a06523f632691fae6006bac1d1364
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-12 14:09:38 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -38,6 +38,19 @@ def app():
       st.header('Display Player Stats of Selected Team(s)')
       st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
       st.dataframe(df_selected_team)
+      # Heatmap
+      if st.button('Intercorrelation Heatmap'):
+            st.header('Intercorrelation Matrix Heatmap')
+            df_selected_team.to_csv('output.csv',index=False)
+            df = pd.read_csv('output.csv')
+
+      corr = df.corr()
+      mask = np.zeros_like(corr)
+      mask[np.triu_indices_from(mask)] = True
+      with sns.axes_style("white"):
+            f, ax = plt.subplots(figsize=(7, 5))
+            ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+      st.pyplot()
 
       
       data_file=st.file_uploader("Upload File",type=['csv','txt','docx','pdf'])
