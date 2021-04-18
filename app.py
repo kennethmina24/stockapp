909f656afcb38dd1fe4699d9b413d1f618cda66f
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-18 16:21:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ Computer Science-Automate My Life
 
 # Add all your application here
 app.add_app("Stock Market", stock.app)
-#app.add_app("DraftKings", NBA.app)
+app.add_app("DraftKings", NBA.app)
 # The main app
 app.run()
