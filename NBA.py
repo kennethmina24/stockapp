b541a60d9d28eed81ff99c6a20d2c8d5a219ae34
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-18 19:47:46 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -72,7 +72,7 @@ def app():
             
             #st.dataframe(df_updated)
             #Combine List
-            Combined_DK_Website=pd.merge([pf,playerstats],how="outer",right_by=None)
+            Combined_DK_Website=pd.merge([pf,playerstats],how="outer",right=None)
             
             st.dataframe(Combined_DK_Website)
        
