2325974aacc712431c4499c0cca05f88e2ae1603
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-18 16:39:05 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -67,7 +67,7 @@ def app():
                    #Show Data
             st.write(file_details)
                      # Filtering data    
-            pf=pf.groupby(by='Name').sum()
+            pf=pf.groupby(by='Name')
             #df_updated = pf[(pf.Pos.isin(selected_pos))]
             
             #st.dataframe(df_updated)
