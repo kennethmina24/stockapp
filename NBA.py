7a07ffb150924427813c5fa031ad247bb0315ec6
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-20 12:16:43 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -61,7 +61,7 @@ def app():
             st.write("Enter File, the data set is empty")
       if data_file is not None:
             pf = pd.read_csv(data_file)
-            pf=pf.drop(columns=['TeamAbbrev','Name + ID','Game Info','Pos_x'])
+            pf=pf.drop(columns=['TeamAbbrev','Name + ID','Game Info','Roster Position'])
             file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
             pf=pf.rename(columns={'Position':'Pos'})
                    #Show Data
