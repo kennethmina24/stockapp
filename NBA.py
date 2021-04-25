657e1d40f86062f6e4c025dc2c43b64072edcd66
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-04-25 12:07:40 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -69,7 +69,7 @@ def app():
             st.write(file_details)
                      # Filtering data    
             #Combine List
-            Combined_DK_Website=pf.merge( df_selected_team,how='outer',on='Name')
+            Combined_DK_Website=pf.merge( playerstats,how='outer',on='Name')
             Combined_DK_Website=Combined_DK_Website.drop(columns= ['Pos_x'])
             Combined_DK_Website=Combined_DK_Website.dropna(subset=['Salary'])
             Combined_DK_Website=Combined_DK_Website.fillna("Empty")
