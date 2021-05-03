ebe35de1f942a8b22ce1b57d218bb819400d7424
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-05-03 12:27:55 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -56,7 +56,6 @@ def app():
             file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
             pf=pf.rename(columns={'Position':'Pos'})
             pf=pf.rename(columns={'TeamAbbrev':'Tm'})
-            pf["Name"] = pf["Name"].str.replace("II", " ")
                    #Show Data
             st.write(file_details)
                      # Filtering data    
