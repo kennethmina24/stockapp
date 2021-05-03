346ce27f890c88f4dcad8410fb3554ae035c8a7c
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-05-03 11:29:59 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -20,7 +20,7 @@ def app():
           playerstats = raw.drop(['Rk','Age','G','GS'], axis=1)
           playerstats=playerstats.rename(columns={'Player':'Name'})
           playerstats=playerstats.drop_duplicates(subset=(['Name']))
-          playerstats["Name"] = playerstats["Name"].str.replace("ć", "3")
+          playerstats["Name"] = playerstats["Name"].str.replace("ć", "c")
           return playerstats
       playerstats = load_data()
       # Filtering data
