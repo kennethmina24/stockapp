77fd842f19c404b69b158702f118f92cc7c8512e
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-05-06 18:49:02 -0400

    Update NBA_Players_Names.py

--- NBA_Players_Names.py
+++ NBA_Players_Names.py
@@ -2,7 +2,8 @@ replace_values={'Danuel House':'Danuel House Jr.',
                 'Kenyon Martin Jr.' :'KJ Martin Jr.',
                 'JaKarr Sampson':'Jakarr Sampson',
                 'Brian Bowen':'Brian Bowen II',
-                'Sviatoslav Mykhailiuk':'Svi Mykhailiuk'
+                'Sviatoslav Mykhailiuk':'Svi Mykhailiuk',
+                'Lonnie Walker':'Lonnie Walker IV'
                } 
 
 #,'Sviatoslav Mykhailiuk':'Svi Mykhailiuk'
