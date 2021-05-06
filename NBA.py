6bc438298bfb7aa62648960dc3ec61a2ea921c6a
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-05-06 18:45:56 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -38,6 +38,7 @@ def app():
           playerstats["Name"] = playerstats["Name"].str.replace("ā", "a")
           playerstats["Name"] = playerstats["Name"].str.replace("Č", "C")
           playerstats["Name"] = playerstats["Name"].str.replace("İ", "I")
+          playerstats["Pos"] = playerstats["Pos"].str.replace("SF-SG", "SG")
           playerstats=playerstats.replace({'Name':replace_values})
     
             
