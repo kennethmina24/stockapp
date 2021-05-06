12b0fd7c4b5d187ad8007b4c8354d98046ac65b4
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-05-06 17:50:19 -0400

    Update NBA_Players_Names.py

--- NBA_Players_Names.py
+++ NBA_Players_Names.py
@@ -1,3 +1,3 @@
-replace_values = {'Ennis':'Enis', 'Medium':2, 'High':3 }  
+replace_values = {'Danuel House':'Danuel House Jr.', 'Medium':2, 'High':3 }  
 
 #state_df = state_df.replace({"state": replace_values})
