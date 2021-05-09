e470872296311bdfa7d5183e7370778f01828b11
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-05-09 19:18:42 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -67,12 +67,13 @@ def app():
             Combined_DK_Website=pf.merge( playerstats,how='outer',on='Name')
             Combined_DK_Website=Combined_DK_Website.drop(columns= ['Pos_x'])
             Combined_DK_Website=Combined_DK_Website.dropna(subset=['Salary'])
+            Combined_DK_Website = Combined_DK_Website[Combined_DK_Website.Name !='Udonis Haslem' ]
             Combined_DK_Website=Combined_DK_Website.fillna('Empty')
             #Combined_DK_Website['Points/Minutes']=Combined_DK_Website['AvgPointsPerGame']/Combined_DK_Website['MP']
             Combined_DK_Website['MP'] =  Combined_DK_Website.MP.astype(float)
             x=Combined_DK_Website.dtypes
             st.dataframe(x)
-            Combined_DK_Website = Combined_DK_Website[Combined_DK_Website.Name !='Udonis Haslem' ]
+            
              #Sidebar - Team selection
             sorted_unique_team = sorted(Combined_DK_Website.Tm.unique())
             selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
