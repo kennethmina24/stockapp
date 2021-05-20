34d0625eadd8b0e6ccae8d4da3c5abc3c27c99b9
kennethmina24 <53905428+kennethmina24@users.noreply.github.com>
2021-05-20 12:42:35 -0400

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -52,10 +52,14 @@ def app():
       st.header('Display Player Stats of Selected Team(s)')
       #st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
       st.dataframe(playerstats)
+      def filedownload(df):
+                csv = df.to_csv(index=False)
+                b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+                href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+                return href
+      st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
       API_Key='erGvaGFQ04PUhHoIbCbXoB6Gy'
         #Twitter
-
-     
       # Mine Data
       data_file=st.file_uploader("Upload File",type=['csv','txt','docx','pdf'])
       if data_file is None:
@@ -98,13 +102,7 @@ def app():
             #st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
             #st.dataframe(df_selected_team)
             st.dataframe(Combined_DK_Website)
-            def filedownload(df):
-                csv = df.to_csv(index=False)
-                b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-                href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
-                return href
-
-            st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+            
             
              # Heatmap
             if st.button('Intercorrelation Heatmap'):
